3d0a086cecdb817abc7d4f74b2271c50100962c0
Danijel Korent <danijel.korent@gmail.com>
2018-10-29 18:40:49 +0100

    Added skeleton code for startup operations

---
+++ source_code/startup.asm
@@ -0,0 +1,23 @@
+
+; INFO:
+;  - https://www.gnu.org/software/grub/manual/multiboot/multiboot.html
+;
+
+MULITBOOT_FLAGS equ 0x0
+
+global loader ;GRUB will jump into this function
+
+section .text ; start of C memory segment for code
+
+    ; per multiboot specification. Without this GRUB will not recognite this binary image as bootable
+    align 4
+    dd 0x1BADB002 ;multiboot magic number
+    dd MULITBOOT_FLAGS
+    dd -(0x1BADB002 + MULITBOOT_FLAGS) ; when be zero when magic_number+multiboot flags are summed with it
+
+
+loader:
+    mov eax, 0xFEEDBEEF ;a clasic
+
+.loop:
+    jmp .loop ;infinite loop
