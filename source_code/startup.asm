23dfda408ab2d092ceaf9a36d387cdce0eb6ba3a
Danijel Korent <danijel.korent@gmail.com>
2018-10-29 21:24:07 +0100

    Minor modifications missing from previous commit :)

--- source_code/startup.asm
+++ source_code/startup.asm
@@ -1,13 +1,14 @@
 
 ; INFO:
+;  - https://en.wikibooks.org/wiki/X86_Assembly/NASM_Syntax
 ;  - https://www.gnu.org/software/grub/manual/multiboot/multiboot.html
 ;
 
 MULITBOOT_FLAGS equ 0x0
 
-global loader ;GRUB will jump into this function
+global start_kernel ;GRUB will jump into this function
 
-section .text ; start of C memory segment for code
+section .text ; standard name of the C memory segment for code
 
     ; per multiboot specification. Without this GRUB will not recognite this binary image as bootable
     align 4
@@ -16,8 +17,8 @@ section .text ; start of C memory segment for code
     dd -(0x1BADB002 + MULITBOOT_FLAGS) ; when be zero when magic_number+multiboot flags are summed with it
 
 
-loader:
-    mov eax, 0xFEEDBEEF ;a clasic
+start_kernel:
+    mov eax, 0xFEEDBEEF ;a classic
 
 .loop:
     jmp .loop ;infinite loop
