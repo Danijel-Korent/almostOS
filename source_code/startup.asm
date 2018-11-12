8177163a5ae43fbc2f3982fdcf3dc935ca715685
Danijel Korent <danijel.korent@gmail.com>
2018-11-12 19:55:04 +0100

    Added printing out a 'Starting kernel...' message in a nice green color

--- source_code/startup.asm
+++ source_code/startup.asm
@@ -2,6 +2,7 @@
 ; INFO:
 ;  - https://en.wikibooks.org/wiki/X86_Assembly/NASM_Syntax
 ;  - https://www.gnu.org/software/grub/manual/multiboot/multiboot.html
+;  - http://kernfunny.org/x86/ (x86 Instruction Set Reference)
 ;
 
 MULITBOOT_FLAGS equ 0x0
@@ -16,9 +17,28 @@ section .text ; standard name of the C memory segment for code
     dd MULITBOOT_FLAGS
     dd -(0x1BADB002 + MULITBOOT_FLAGS) ; when be zero when magic_number+multiboot flags are summed with it
 
+hello_msg:
+	db "Starting AlmostOS kernel...", 0
 
 start_kernel:
     mov eax, 0xFEEDBEEF ;a classic
+
+    mov esi, hello_msg
+    mov edi, 0x000B8000
+
+.print_hello: 		;shit just got real
+    mov cl, [esi]
+    jcxz .loop 		;stop printing on null-terminator
+
+    mov byte [edi], cl
+    inc edi
+
+    mov byte [edi], 0x02 ; Cool looking green with the black backgroud
+    inc edi
+
+    inc esi
+    jmp .print_hello
+
 
     ; NEXT TODO: to compile and jumpt to C program, need to
     ;   - Reserverve the memory space for stack
