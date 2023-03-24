2d6ff894d9ef63e6d83878feb410184e646557aa
Danijel Korent <danijel.korent@gmail.com>
2023-03-24 07:52:27 +0100

    Created file shell.c and moved related code from terminal.c into it

---
+++ source_code/shell.h
@@ -0,0 +1,8 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+#include "kernel_stddef.h"
+
+void shell_input(u8 * input);
+
+#endif // _SHELL_H_
--- source_code/terminal.c
+++ source_code/terminal.c
@@ -7,6 +7,7 @@
 
 #include "terminal.h"
 
+#include "shell.h"
 #include "string.h"
 #include "kernel_main.h"
 #include "kernel_stdio.h"
@@ -18,7 +19,6 @@
 
 // Local function defitions
 static void terminal_clear_input_line(terminal_contex_t *terminal_context);
-void input_callback(char * input);
 
 
 // TODO: Add argument for no-input mode
@@ -225,7 +225,7 @@ void terminal_on_keypress(terminal_contex_t *terminal_context, unsigned char key
         // Copy the line
         // Pass the line to callback_process_input()
         // I should really decouple this from terminal_on_keypress()
-        input_callback(terminal_context->input_line + 8);
+        shell_input(terminal_context->input_line + 8);
 
         terminal_clear_input_line(terminal_context);
     }
@@ -250,124 +250,4 @@ static void terminal_clear_input_line(terminal_contex_t *terminal_context)
     }
 
     terminal_context->input_cursor_position = 8;
-}
-
-
-// --------------------------------- TODO: Move to different file -----------------------------------/
-
-#include "hexdump.h"
-#include "kernel_main.h"
-
-/**
- * @brief Parse raw input string and returns it in argv/argc format (array of strings/arguments)
- *
- * @param input_string The raw input string taken from command-line
- * @param argc Number of arguments(strings) in the array "argv"
- * @return char** Returns "argv", which is an array of string pointers
- *
- * @note The function just reuses and modifies input string, and all argv pointers
- *       point to a memory space to which "input_string" is pointing
- */
-char** parse_arguments(char* input_string, int* argc)
-{
-    if (input_string == NULL || argc == NULL)
-    {
-        LOG("ERROR: parse_arguments() - received NULL argument!");
-        return NULL;
-    }
-
-    char* trimmed_input = trim_string(input_string);
-
-    if (trimmed_input == NULL) return NULL;
-
-    // Count the number of args, by counting whitespaces between words,
-    // we need to know the length of the array before allocating it on heap
-    int len = strlen_unsafe(trimmed_input);
-    int num_of_args = 0;
-
-    for(int i = 0; i < len; i++)
-    {
-        // Find whitespace
-        if( trimmed_input[i] == ' ' ) num_of_args++;
-
-        // Skip all consecutive whitespaces
-        for(; trimmed_input[i] == ' '; i++);
-    }
-
-    *argc = num_of_args +1; // +1 because cmd (first word) is also in the list
-
-    // Allocate argv array
-    char** argv = malloc(*argc * sizeof(const char*));
-
-    if (argv == NULL)
-    {
-        LOG("ERROR: parse_arguments() - No free memory!");
-    }
-    else
-    {
-        char *string_start = trimmed_input;
-        char *string_end =   trimmed_input;
-
-        for(int i = 0; i < *argc; i++)
-        {
-            // First whitespace symbol marks the end of argument
-            for(;(*string_end != ' ') && (*string_end != 0); string_end++);
-
-            // Null-terminate after every word
-            // because they will be used in-place
-            *string_end = 0;
-
-            argv[i] = string_start;
-
-            // Skip null-terminator and all consecutive whitespaces
-            string_end++;
-            for(; *string_end == ' '; string_end++);
-
-            string_start = string_end;
-        }
-    }
-
-    return argv;
-}
-
-
-void kernel_println(const unsigned char* const message); // TOOD: typo "stdandard"
-
-
-// TODO: Temp code for testing!
-void input_callback(char * input)
-{
-    if (input == NULL) return;
-
-    int    argc = 0;
-    char** argv = NULL;
-
-    argv = parse_arguments(input, &argc);
-
-    if (argv == NULL)
-    {
-        kernel_println("ERROR: Parsing arguments failed");
-        return;
-    }
-
-    if (input[0] == 'h')
-    {
-        kernel_println("");
-        kernel_println("Available commands:");
-        kernel_println("");
-        kernel_println("    help - prints this message");
-        kernel_println("");
-    }
-    if (input[0] == 'd')
-    {
-        execute__dump_data(argc, argv);
-    }
-    else
-    {
-        kernel_println("");
-        kernel_println("Unknown command!");
-        kernel_println("");
-    }
-
-    free(argv);
 }
