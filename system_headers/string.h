1cbc65233791c22923fca47da8b28ef77ec2dea3
Danijel Korent <danijel.korent@gmail.com>
2025-08-02 07:52:19 +0200

    system_header/string.h: Added prototype of strncmp() used by fs/tinyfs/code/rootdir_cluster.c

--- system_headers/string.h
+++ system_headers/string.h
@@ -5,6 +5,9 @@
 
 #include <stdint.h>
 
+// TODO: I should probably move folder "system_headers" to "stdlib_c/headers"
+
 uint32_t strlen(const char *str);
+int strncmp(const char *str1, const char *str2, uint32_t n);
 
 #endif // _STRING_H_
