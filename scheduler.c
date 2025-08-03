9620cb20e88cb0aab54885ba3d3a3fdbc408a71d
Danijel Korent <danijel.korent@gmail.com>
2025-08-03 14:09:00 +0200

    scheduler.c: Started implementing round-robin scheduler

--- scheduler.c
+++ scheduler.c
@@ -6,24 +6,43 @@
 
 ///////////////////////////////////// Scheduler test code /////////////////////////////////////
 
+static void create_process(struct process_ctx *new_process, const char* name, void (*func_ptr)(void));
 
+static void test_thread2_handler(void);
+static void test_thread3_handler(void);
+static void test_thread4_handler(void);
+
+static struct process_ctx process_list[16] = {0}; // 16 processes ought to be enough for everyone (I need to stop making this jokes)
+
+static int process_list_index = 0;
 
 static struct process_ctx *current_running_process = NULL;
 static struct process_ctx kernel_process = {0};
 static struct process_ctx test_process = {0};
 
-void test_thread2_handler(void);
 
 void scheduler_init(void)
 {
     kernel_println("Called scheduler_init()");
     //kernel_printf("Called scheduler_init() --> %s \n", "TEST");
 
+#if 0
     kernel_process.name     = "Kernel process";
     current_running_process = &kernel_process;
 
     // TEMP - test code
     create_process(&test_process, "Test process", test_thread2_handler);
+#else
+    kernel_process.name     = "Kernel process";
+    process_list[0] = kernel_process;
+    //mem_copy(&process_list[0], sizeof(process_list[0]), kernel_process, sizeof(struct process_ctx));
+
+    current_running_process = &process_list[0];
+
+    //create_process(&process_list[1], "Test process 2", test_thread2_handler);
+    //create_process(&process_list[4], "Test process 3", test_thread3_handler);
+    create_process(&process_list[15], "Test process 4", test_thread4_handler);
+#endif
 }
 
 void schedule(void)
@@ -32,6 +51,7 @@ void schedule(void)
     struct process_ctx *process_to_restore = NULL;
     struct process_ctx *process_to_save    = current_running_process;
 
+#if 0
     if (current_running_process == &kernel_process)
     {
         process_to_restore = &test_process;
@@ -40,6 +60,28 @@ void schedule(void)
     {
         process_to_restore = &kernel_process;
     }
+#else
+    process_to_restore = current_running_process;
+
+    int i = process_list_index;
+
+    while(1)
+    {
+        i++;
+        i = i % (sizeof(process_list) / sizeof(process_list[0]));
+
+        if (process_list[i].name == NULL)
+        {
+            continue;
+        }
+        else
+        {
+            process_to_restore = &process_list[i];
+            process_list_index = i;
+            break;
+        }
+    }
+#endif
 
     current_running_process = process_to_restore;
 
@@ -52,7 +94,20 @@ void schedule(void)
     switch_process(process_to_save, process_to_restore);
 }
 
-void create_process(struct process_ctx *new_process, const char* name, void (*func_ptr)(void))
+
+// This is really just a pointer that grows in one direction, but never shrinks, so it cannot deallocate space
+// Good enough for initial testing, but we cannot dinamically create and delete processes because the pointer
+// would quickly run out of memory
+// TODO:
+//      - Use heap_malloc()
+//          - For that I need to add support for multiple bin sizes (I need to do that anyway)
+//              or
+//          - much faster solution would be to make allocator instance based, and just make new instace with PROCESS_STACK_SIZE bins
+static int poors_man_stack_allocator = 0;
+
+
+// TODO: This is x86 specific code, move to arch/x86-32
+static void create_process(struct process_ctx *new_process, const char* name, void (*func_ptr)(void))
 {
     kernel_println("Called init_process()");
 
@@ -63,8 +118,32 @@ void create_process(struct process_ctx *new_process, const char* name, void (*fu
 
     u32 stack_start_addr = 0;
 
-    stack_start_addr = (u32)&dummy_var - (128 * 1024);         // A hack until I implement proper stack mem management
-    stack_start_addr = stack_start_addr & 0xFFFF0000;    // Align the address to 64K boundary for easier reading
+#if 0
+    stack_start_addr = (u32)&dummy_var - (128 * 1024);  // TODO: A hack until I implement proper stack mem management
+    stack_start_addr = stack_start_addr & 0xFFFF0000;   // Align the address to 64K boundary for easier reading
+#else
+
+    #define PROCESS_STACK_SIZE (128 * 1024)
+
+    if (poors_man_stack_allocator == 0)
+    {
+        poors_man_stack_allocator = (u32)&dummy_var - PROCESS_STACK_SIZE;       // TODO: A hack until I implement proper stack memomory management
+        poors_man_stack_allocator = poors_man_stack_allocator & 0xFFFF0000;     // Align the address to 64K boundary for easier reading/debugging
+    }
+    else
+    {
+        poors_man_stack_allocator -= PROCESS_STACK_SIZE;
+    }
+
+    if (poors_man_stack_allocator <= PROCESS_STACK_SIZE)
+    {
+        kernel_printf("[ERROR] create_process(): No more stack space! poors_man_stack_allocator = %x", poors_man_stack_allocator);
+        return;
+    }
+
+    stack_start_addr = (u32)poors_man_stack_allocator;
+#endif
+
 
     u32 *process_stack = (u32*) stack_start_addr;
 
@@ -96,19 +175,19 @@ void create_process(struct process_ctx *new_process, const char* name, void (*fu
     kernel_printf("init_process():   SP    = %x \n", new_process->reg_esp);
 }
 
-static int test_thread2_counter = 0;
-
-void test_thread2_handler(void)
+static void thread_test(const char* func_name, u32 counter_target)
 {
-    kernel_println("\n>>>>>>>>>> Start of test_thread2_handler() <<<<<<<<<<< \n");
+    kernel_printf("\n>>>>>>>>>> Start of %s() <<<<<<<<<<< \n", func_name);
+
+    int test_thread2_counter = 0;
 
     while(1)
     {
-        u32 counter = 1000 * 1000;
+        //u32 counter = 1000 * 1000;
 
-        if (test_thread2_counter % counter == 1)
+        if (test_thread2_counter % counter_target == 1)
         {
-            kernel_println("!!! TEST THREAD_2 RUNNING !!!");
+            kernel_printf("[%s] Still running... ", func_name);
         }
     
         test_thread2_counter++;
@@ -116,6 +195,22 @@ void test_thread2_handler(void)
         schedule();
     }
 }
+
+static void test_thread2_handler(void)
+{
+    thread_test(__PRETTY_FUNCTION__, (1000*1000));
+}
+
+static void test_thread3_handler(void)
+{
+    thread_test(__PRETTY_FUNCTION__, (2*1000*1000));
+}
+
+static void test_thread4_handler(void)
+{
+    thread_test(__PRETTY_FUNCTION__, (4*1000*1000));
+}
+
 
 ///////////////////////////////////// /Scheduler test code /////////////////////////////////////
 
